57b303eb2b2728c5f4bfb4e5e6b46d0e209db323
cho.oh <cho.oh@bizreach.co.jp>
2018-06-29 18:50:43 +0900

    until chapter 3

---
+++ src/3-1.kt
@@ -0,0 +1,37 @@
+import java.util.*
+
+/**
+ * @author cho.oh 2018/06/20.
+ */
+
+//Basic Task
+//Create a new Kotlin file.
+//Copy and paste the main() function from Hello World into the file.
+//Create a new function, dayOfWeek().
+//In the body of the function, print "What day is it today?"
+//Call dayOfWeek() from main().
+//Run your program.
+
+
+//
+fun main(args: Array<String>) {
+//    dayOfWeek()
+}
+
+//fun dayOfWeek() {
+//    println("What day is it today?")
+//}
+
+//fun dayOfWeek() {
+//    val currentDay = when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
+//        1 -> "Sunday"
+//        2 -> "Monday"
+//        3 -> "Tuesday"
+//        4 -> "Wednesday"
+//        5 -> "Thursday"
+//        6 -> "Friday"
+//        7 -> "Saturday"
+//        else -> "Time has stopped"
+//    }
+//    println(currentDay)
+//}
---
+++ src/3-5.kt
@@ -0,0 +1,12 @@
+/**
+ * @author cho.oh 2018/06/22.
+ */
+fun main(args: Array<String>) {
+    println(whatShouldIDoToday("sad"))
+}
+fun whatShouldIDoToday(mood: String, weather: String = "sunny", temperature: Int = 24): String {
+    return when {
+        mood == "happy" && weather == "Sunny" -> "go for a walk"
+        else -> "Stay home and read"
+    }
+}
