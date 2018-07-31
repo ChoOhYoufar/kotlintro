20719f8f051caa244fafe0e1328e2a59c87a0582
cho.oh <cho.oh@bizreach.co.jp>
2018-07-31 19:04:13 +0900

    add pair part

--- src/essential/5-1_pair.kt
+++ src/essential/5-1_pair.kt
@@ -3,6 +3,10 @@ package essential
 /**
  * @author cho.oh 2018/07/27.
  */
+//Pair Triple
+//Destructuring　同時に複数の変数を作る
+//関数の返す値を複数にできる
+//mapを
 
 class Book(val title: String, val author: String, val year: Int) {
     fun getTitleAndAuthor(title: String, author: String): Pair<String, String> {
@@ -17,5 +21,9 @@ class Book(val title: String, val author: String, val year: Int) {
 fun main(args: Array<String>) {
     val book = Book("X", "Y", 1950)
     val (triple1, triple2, triple3) = book.getTitleAuthorYear()
+    //triple, pairをStringに変換できる
+    val titleAuthorYearStr = book.getTitleAuthorYear().toString()
+    //triple, pairをListに変換できる
+    val titleAuthorYearList = book.getTitleAuthorYear().toList()
     println("Here is your book $triple1 written by $triple2 in $triple3")
 }
